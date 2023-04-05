d99b0e16ed77f9eadd04e8256a3b155ae4d47cae
Gioele-Bio <gioelemontis@biospyder.com>
2023-04-05 11:08:04 +0100

    installation script workaround

--- local_dash_app/install.sh
+++ local_dash_app/install.sh
@@ -1,5 +1,6 @@
 # Determine installation path and install conda there
-installationPath=$(pwd)
+#installationPath=$(pwd)
+installationPath=$HOME/temposeq_aligner_assets
 
 # NEED TO ADD THE PATH TO CONDA INTERPRETER IN ALIGN FILES LOCAL, NOT SURE IF PASSED BY APP.PY OR STRAIGHT IN THERE
 # Possibly need to modify conda script or iteratively run sed 
@@ -14,9 +15,10 @@ if test -f "./source/conda/bin/conda"; then
 else
     echo "Installing miniconda"
     cd source
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p ./conda >> installation.log 2>&1
+    echo $installationPath/conda
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $installationPath/conda >> installation.log 2>&1
     cd ../
-    if test -f "./source/conda/bin/conda"; then
+    if test -f $installationPath/conda/bin/conda; then
         echo "Miniconda was installed successfully"
     else
         echo "An error has occurred while installing conda, please check the log file"
@@ -25,7 +27,7 @@ fi
 
 
 # Install dash and pandas with conda
-./source/conda/bin/pip install dash pandas >> ./source/installation.log
+$installationPath/conda/bin/pip install dash pandas >> ./source/installation.log
 echo "Installed dash and pandas"
 echo " "
 
@@ -33,19 +35,19 @@ echo " "
 
 # Start installing samtools
 # Make a list of installed packages
-./source/conda/bin/conda list > condaList
+$installationPath/conda/bin/conda list > condaList
 if grep -Fq "samtools" condaList; then
     echo "Samtools was already installed"
 else
     echo "Installing Samtools...."
-    ./source/conda/bin/conda install -c bioconda -y samtools >> ./source/installation.log
+    $installationPath/conda/bin/conda install -c bioconda -y samtools >> ./source/installation.log
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! this im not sure we do need
     # cd source/conda/lib/
     # ln -s librcrypto.so.1.1 libcrypto.so.1.0.0
     # cd ../../../
 
     # Check if it was installed
-    ./source/conda/bin/conda list > condaList
+    $installationPath/conda/bin/conda list > condaList
     if grep -Fq "samtools" condaList; then
         echo "Samtools was installed successfully"
     else
@@ -57,15 +59,15 @@ rm condaList
 
 # Install featureCount (subread)
 # Make a list of installed packages
-./source/conda/bin/conda list > condaList
+$installationPath/conda/bin/conda list > condaList
 if grep -Fq subread condaList; then
     echo "featureCount is already installed"
 else
     echo "Installing featureCount...."
-    ./source/conda/bin/conda install -c bioconda -y subread >> ./source/installation.log
+    $installationPath/conda/bin/conda install -c bioconda -y subread >> ./source/installation.log
 
     # Check if it was installed
-    ./source/conda/bin/conda list > condaList
+    $installationPath/conda/bin/conda list > condaList
     if grep -Fq subread condaList; then
         echo "featureCount was installed successfully"
     else
@@ -76,17 +78,17 @@ rm condaList
 
 # Install STAR
 # Make a list of installed packages
-./source/conda/bin/conda list > condaList
+$installationPath/conda/bin/conda list > condaList
 echo ""
 echo "Checking STAR"
 if grep -Fq star condaList; then
     echo "STAR is already installed"
 else
     echo "Installing STAR"
-    ./source/conda/bin/conda install -c bioconda -y star >> ./source/installation.log
+    $installationPath/conda/bin/conda install -c bioconda -y star >> ./source/installation.log
     
     # Check if installed successfully
-    ./source/conda/bin/conda list > condaList
+    $installationPath/conda/bin/conda list > condaList
     if grep -Fq star condaList; then
         echo "STAR was installed successfully"
     else
@@ -97,17 +99,17 @@ rm condaList
 
 
 # Install BWA
-./source/conda/bin/conda list > condaList
+$installationPath/conda/bin/conda list > condaList
 echo ""
 echo "Checking BWA"
 if grep -Fq bwa condaList; then
     echo "BWA is already installed"
 else
     echo "Installing BWA"
-    ./source/conda/bin/conda install -c bioconda -y bwa >> ./source/installation.log
+    $installationPath/conda/bin/conda install -c bioconda -y bwa >> ./source/installation.log
     
     # Check if installed successfully
-    ./source/conda/bin/conda list > condaList
+    $installationPath/conda/bin/conda list > condaList
     if grep -Fq bwa condaList; then
         echo "BWA was installed successfully"
     else
@@ -119,17 +121,17 @@ rm condaList
 
 
 # Install Kallisto
-./source/conda/bin/conda list > condaList
+$installationPath/conda/bin/conda list > condaList
 echo ""
 echo "Checking Kallisto"
 if grep -Fq kallisto condaList; then
     echo "Kallisto is already installed"
 else
     echo "Installing Kallisto"
-    ./source/conda/bin/conda install -c bioconda -y kallisto >> ./source/installation.log
+    $installationPath/conda/bin/conda install -c bioconda -y kallisto >> ./source/installation.log
     
     # Check if installed successfully
-    ./source/conda/bin/conda list > condaList
+    $installationPath/conda/bin/conda list > condaList
     if grep -Fq kallisto condaList; then
         echo "Kallisto was installed successfully"
     else
